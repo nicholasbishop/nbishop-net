ccff42e5dc093d383169128e90b96f9fd97bc19e
Nicholas Bishop <nbishop@nbishop.net>
2021-12-16 00:32:54 -0500

    Implement publish

---
+++ src/credential_helper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo username="nicholasbishop"
+echo password="${TOKEN}"
--- src/publish.rs
+++ src/publish.rs
@@ -1,5 +1,82 @@
 use anyhow::Result;
+use command_run::Command;
+use fs_err as fs;
+use std::env;
+use std::path::Path;
 
 pub fn publish() -> Result<()> {
-    todo!();
+    // Setup notes:
+    // - Create a personal access token with the `public_repo` scope
+    // - repo settings > Environments > github-pages > Add secret
+    //   - Name: TOKEN
+
+    let branch = "gh-pages";
+    let url = "https://github.com/nicholasbishop/nbishop-net.git";
+    let repo_path = "repo";
+
+    // Delete the repo if it already exists.
+    if Path::new(repo_path).exists() {
+        fs::remove_dir_all(repo_path)?;
+    }
+
+    // Create an empty repo.
+    Command::with_args("git", &["init", repo_path]).run()?;
+
+    // Configure the credential helper. The credential helper helps
+    // avoid showing the private token.
+    let helper_path = env::current_dir()?.join("src/credential_helper.sh");
+    Command::with_args(
+        "git",
+        &[
+            "-C",
+            repo_path,
+            "config",
+            "credential.helper",
+            helper_path.to_str().unwrap(),
+        ],
+    )
+    .run()?;
+
+    // Add the remote.
+    Command::with_args(
+        "git",
+        &["-C", repo_path, "remote", "add", "origin", url],
+    )
+    .run()?;
+
+    // Fetch the branch.
+    Command::with_args("git", &["-C", repo_path, "fetch", "origin", branch])
+        .run()?;
+
+    // Check out the branch.
+    Command::with_args("git", &["-C", repo_path, "checkout", branch]).run()?;
+
+    // Delete all the files currently present. Allow this to fail in
+    // case there's nothing there yet.
+    Command::with_args("git", &["-C", repo_path, "rm", "*"])
+        .disable_check()
+        .run()?;
+
+    // Copy all the output files into the repo.
+    // TODO: deduplicate path.
+    let output_dir = "output";
+    for entry in fs::read_dir(output_dir)? {
+        let entry = entry?;
+        fs::copy(entry.path(), Path::new(repo_path).join(entry.file_name()))?;
+    }
+
+    // Add all the files.
+    Command::with_args("git", &["-C", repo_path, "add", "*"]).run()?;
+
+    // Commit.
+    Command::with_args(
+        "git",
+        &["-C", repo_path, "commit", "-m", "Automatic publish"],
+    )
+    .run()?;
+
+    // Push.
+    Command::with_args("git", &["-C", repo_path, "push"]).run()?;
+
+    Ok(())
 }
